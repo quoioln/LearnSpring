441a4ce16d6dc5a1d84b9a2e09642e8288473ef7
Quoi Vo Phu <vpquoi@tma.com.vn>
2016-08-09 16:30:13 +0700

    update

---
+++ SpringSample/src/com/quoioln/model/StudentData.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.quoioln.model;
+
+/**
+ * @author vpquoi
+ *
+ */
+public class StudentData {
+    private Student student1;
+    private Student student2;
+    /**
+     * @return the student1
+     */
+    public final Student getStudent1() {
+        return student1;
+    }
+    /**
+     * @param student1 the student1 to set
+     */
+    public final void setStudent1(Student student1) {
+        this.student1 = student1;
+    }
+    /**
+     * @return the student2
+     */
+    public final Student getStudent2() {
+        return student2;
+    }
+    /**
+     * @param student2 the student2 to set
+     */
+    public final void setStudent2(Student student2) {
+        this.student2 = student2;
+    }
+    
+    
+}
