a6840f3d0d2da7a56c7990d1c4190f5326c575bb
quoioln <quoipro94@gmail.com>
2016-08-30 02:03:36 +0700

    update

---
+++ EmployeeManament2/src/main/java/com/quoioln/example/config/WebConfiguration.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.quoioln.example.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * @author quoioln
+ *
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfiguration extends WebMvcConfigurerAdapter {
+
+	 @Override
+	    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+	        converters.add(mappingJackson2HttpMessageConverter());
+	    }
+	     
+	    @Bean
+	    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+	        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+	        converter.setObjectMapper(new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false));
+	        return converter;
+	    }
+}
---
+++ EmployeeManament2/src/main/java/com/quoioln/example/form/UpdateProjectData.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package com.quoioln.example.form;
+
+import java.util.List;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class UpdateProjectData.
+ *
+ * @author quoioln
+ */
+public class UpdateProjectData {
+	
+	/** The employee id. */
+	private String employeeId;
+	
+	/** The project id list. */
+	private List<String> projectIdList;
+	
+	/**
+	 * Gets the employee id.
+	 *
+	 * @return the employeeId
+	 */
+	public final String getEmployeeId() {
+		return employeeId;
+	}
+	
+	/**
+	 * Sets the employee id.
+	 *
+	 * @param employeeId the employeeId to set
+	 */
+	public final void setEmployeeId(String employeeId) {
+		this.employeeId = employeeId;
+	}
+	
+	/**
+	 * Gets the project id list.
+	 *
+	 * @return the projectIdList
+	 */
+	public final List<String> getProjectIdList() {
+		return projectIdList;
+	}
+	
+	/**
+	 * Sets the project id list.
+	 *
+	 * @param projectIdList the projectIdList to set
+	 */
+	public final void setProjectIdList(List<String> projectIdList) {
+		this.projectIdList = projectIdList;
+	}
+	
+	
+}	
